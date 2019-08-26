994f598948d661c722a794c72da8a888bbd52452
elk1181 <49888757+elk1181@users.noreply.github.com>
2019-08-25 20:58:03 -0400

    Update A1Novice.java

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -8,7 +8,51 @@ public class A1Novice {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		
+	int customers = scan.nextInt();
+	int i;
+	String fname = null;
+	String lname = null;
+	double total = 0.0;
+	String array[]= new String [customers];
+	
+
+		for( i = 0; i<customers; i++ ) {
+			
+			
+			fname = scan.next();
+		
+			 lname = scan.next();
+
+			
+			int numItems = scan.nextInt();
+			
+			
+		
+			for (int k = 0; k<numItems; k++) {
+				
+			
+				int quanity = scan.nextInt();
+		
+			    String item = scan.next();
+			
+			    double pri = scan.nextDouble();
+			   total = total + (pri * quanity);
+			   array[i] = fname.charAt(0)+ "."+  lname + ':'+" " + Double.toString(total);
+			
+			
+			}
+		total = 0;
+		
+	if (i==(customers-1)) {
+	for(int g = 0; g<customers; g++) {
+		System.out.println(array[g]);
+	}
+	
+	}
+	
+	
+		}
 		
 	}
 }
